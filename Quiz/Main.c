274988847a888c0d5deb84571ce7894af99ef366
morghayn <morgan.nolan@hotmail.com>
2020-04-21 13:44:53 +0100

    Further developed skeleton / plan for classes.

--- Quiz/Main.c
+++ Quiz/Main.c
@@ -1,25 +1,11 @@
-int main()
-{
-	// at start
-			// load questions from file
-			// print out number of questions from file
-	
-	// start asking questions
-			// for each question, display a clue (if available, see below); the user has one attempt only, and the program is case insensitive (accepts both lower and upper case answers) 
-			// after each question display the number of questions asked and the number of correctly answered questions so far. Example: Score: 5/7 
-	
-	// at the end store the results in a file quiz_history.txt by appending to the file one line containing:
-			// name of the quiz file
-			// number of questions answered correctly
-			// total number of questions
-			// difficulty level
+﻿#include <stdio.h>
+#include <queue.h>
 
-
+int main(void)
+{	
 	// -> Start
 	// Select quiz
 	// Select  
-
-
 
 	while (1 == 1)
 	{
@@ -34,4 +20,81 @@ int main()
 	return 0;
 }
 
+void random()
+{
+	 ///The questions are asked in random order.
+}
 
+// initiation
+void load_question_file()
+{
+	// load questions from file
+}
+
+void print_question_quantity()
+{
+	// print out number of questions from file
+}
+
+// start asking questions
+void generate_clue()
+{
+	int difficulty; // (temporary will be in header file)
+
+	switch (difficulty)
+	{
+		case 1:
+				/*
+				1. No clue is given and only a ‘?’ is shown. 
+				Example: What is the capital of France? ?
+				*/
+				break;
+		case 2:
+				/*
+				2. A set of blank dashes seprated by spaces is displayed, one for every letter in the answer. 
+				Example: What is the capital of France? - - - -
+				*/
+				break;
+		case 3:
+				/*
+				The ﬁrst and the last letters are shown. 
+				Example: What is the capital of France? P - - - S
+				*/
+				break;
+		case 4:
+				/*
+				Two random letters from the answer are shown in their correct position. 
+				Example: What is the capital of France? P - R -
+				*/
+				break;
+		case 5:
+				/*
+				All letters are shown but in random order.
+				Example: What is the capital of France? R S I P A
+				*/
+				break;
+		case 6:
+				/*
+				The type of clue is randomly selected from the 5 types listed above.
+				*/
+				break;
+		default:
+				printf("Something appears to have gone wrong. o,,o")
+				break;
+	}
+	// for each question, display a clue (if available, see below); the user has one attempt only, and the program is case insensitive (accepts both lower and upper case answers) 
+}
+
+void print_score()
+{
+	// after each question display the number of questions asked and the number of correctly answered questions so far. Example: Score: 5/7 
+}
+
+// at the end store the results in a file quiz_history.txt by appending to the file one line containing:
+void store_results()
+{
+	// name of the quiz file
+	// number of questions answered correctly
+	// total number of questions
+	// difficulty level
+}
