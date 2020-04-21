b327822b42a39d1086461ff0094f363a44a64c35
Morgan Nolan <morgan.nolan@users.noreply.github.com>
2020-04-21 01:27:10 +0100

    Refined linked list from previous assignment

--- Quiz/linked_list.c
+++ Quiz/linked_list.c
@@ -24,62 +24,26 @@ struct node* insert_front(struct node* head, char* str)
 
 void print_nodes(struct node* head)
 {
-	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n> printing word list\n");
-	int total_letters = 0, total_words = 0;
-
 	while (head != NULL)
 	{
-		printf("+ %s\tletter count: %d\n", head->str, strlen(head->str));
-		total_words++;
-		total_letters += strlen(head->str);
+		printf(head->str);
 		head = head->next;
 	}
-
-	printf("\nTotal letters:%d\nTotal correct words:%d\n", total_letters, total_words);
-	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-}
-
-int is_word_doop(struct node* head, char* word)
-{
-	while (head != NULL)
-	{
-		if (strlen(word) == strlen(head->str))
-		{
-			int x = 0;
-
-			for (int i = 0; word[i] != '\0'; i++)
-			{
-				if (word[i] != head->str[i])
-				{
-					x++;
-				}
-			}
-
-			if (x == 0)
-			{
-				return 1;
-			}
-		}
-		head = head->next;
-	}
-
-	return -1;
 }
 
 void release_nodes(struct node* head)
 {
 	struct node* temp;
-	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	//printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		printf("> releasing word: '%s'\n", temp->str);
 		free(temp->str);
 		free(temp);
 	}
 
-	printf("\n> all nodes have been released.\n");
-	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	//printf("\n> all nodes have been released.\n");
+	//printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
